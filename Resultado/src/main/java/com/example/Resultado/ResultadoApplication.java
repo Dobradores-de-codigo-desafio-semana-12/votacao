9f40200240cd6c2e12923206a5de387130dd7ad3
Jeferson5641 <jefe.gomes154@gmail.com>
2024-06-07 09:46:48 -0300

    projeto inicial

---
+++ Resultado/src/main/java/com/example/Resultado/ResultadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Resultado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultadoApplication.class, args);
+	}
+
+}
