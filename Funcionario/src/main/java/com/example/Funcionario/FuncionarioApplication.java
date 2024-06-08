59d588738566453a16d395ed2566743f0df248e6
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-06-07 22:15:26 -0300

    Criado Eureka server e Iniciado Spring Actuator

--- Funcionario/src/main/java/com/example/Funcionario/FuncionarioApplication.java
+++ Funcionario/src/main/java/com/example/Funcionario/FuncionarioApplication.java
@@ -2,8 +2,12 @@ package com.example.Funcionario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
+
 public class FuncionarioApplication {
 
 	public static void main(String[] args) {
