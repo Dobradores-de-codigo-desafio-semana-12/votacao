3b0c6c48b5ad0cc2371767f24841ac3fad068de8
Juliana Ransani <juliana.ransani.2@gmail.com>
2024-06-08 19:41:02 -0300

    Implementando o End Point alterar email.

--- Funcionario/src/main/java/com/example/Funcionario/web/controle/FuncionarioControle.java
+++ Funcionario/src/main/java/com/example/Funcionario/web/controle/FuncionarioControle.java
@@ -3,6 +3,7 @@ package com.example.Funcionario.web.controle;
 import com.example.Funcionario.entidade.Funcionario;
 import com.example.Funcionario.servico.FuncionarioServico;
 import com.example.Funcionario.web.dto.FuncionarioCriarDto;
+import com.example.Funcionario.web.dto.FuncionarioEmailDto;
 import com.example.Funcionario.web.dto.FuncionarioRespostaDto;
 import com.example.Funcionario.web.dto.mapa.FuncionarioMapa;
 import jakarta.validation.Valid;
@@ -27,6 +28,11 @@ public class FuncionarioControle {
     public ResponseEntity<FuncionarioRespostaDto> buscarFuncionario(@PathVariable Long id){
         Funcionario fun = funcionarioServico.buscarPorId(id);
         return ResponseEntity.ok(FuncionarioMapa.paraDto(fun));
+    }
+    @PatchMapping("/email/{id}")
+    public ResponseEntity<FuncionarioRespostaDto> atualizarEmail(@PathVariable Long id, @RequestBody FuncionarioEmailDto dto) {
+        Funcionario fun = funcionarioServico.editarEmail(id, dto.getNovoEmail());
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(FuncionarioMapa.paraDto(fun));
     }
 
 }
