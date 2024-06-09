342e22d92426338db96370896aee1011713c25dc
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-06-09 00:21:27 -0300

    Configurado gateway

--- mscloudgateway/src/main/java/com/example/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/com/example/mscloudgateway/MscloudgatewayApplication.java
@@ -2,12 +2,24 @@ package com.example.mscloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MscloudgatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MscloudgatewayApplication.class, args);
+	}
+
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder
+				.routes()
+					.route(r ->r.path("/Funcionario//**").uri("lb//Funcionario"))
+				.build();
 	}
 
 }
